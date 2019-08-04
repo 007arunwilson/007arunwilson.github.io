cb8b8fe1a578d044feff7b7e3d14c8be3baf830c
Arun Wilson <007arunwilson@gmail.com>
2019-08-05 02:02:01 +0530

    A minimilist version created - Social Links - Thumb image

---
+++ precache-manifest.e7067e7744ca4f44af2b99196c5fa756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ca83ddf0c1043f094fe08c1c6fc5d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853d18ecf9cb5182ce5e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f9f3a3ec2a8003eb96db",
+    "url": "/static/js/2.d843c2ab.chunk.js"
+  },
+  {
+    "revision": "853d18ecf9cb5182ce5e",
+    "url": "/static/js/main.7156e767.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5be27c3bfb3087f1905ed5b53dbcb3a5",
+    "url": "/static/media/image.5be27c3b.png"
+  }
+]);
